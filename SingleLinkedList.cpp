1d8d63b5844d1662ecefc574f66bfbb89f984331
annisafathul <annisafathul18@gmail.com>
2026-04-23 16:07:52 +0700

    menambahkan booldelnode untuk menghapus data

--- SingleLinkedList.cpp
+++ SingleLinkedList.cpp
@@ -92,5 +92,22 @@ class LinkedList
             }
             return current != NULL;
         }
+
+        booldelNode(int nim)
+        {
+            Node *current, *previous;
+            if (!search(nim, previous, current))
+                return false;
+
+                if (current == START)
+                    START = START->next;
+                else
+                    previous->next = current->next;
+                
+                    delete current;
+                
+                    return true;
+        }
+
     }
 }
