98f94e11f7d6fa2dc632d7c20c8141ea0f1fe3e5
annisafathul <annisafathul18@gmail.com>
2026-04-23 16:09:18 +0700

    menambah traverse untuk membaca data

--- SingleLinkedList.cpp
+++ SingleLinkedList.cpp
@@ -109,5 +109,23 @@ class LinkedList
                     return true;
         }
 
+        void traverse()
+        {
+            if (listEmpty())
+                cout << "\nList Kosong\n";
+            else
+            {
+                cout << "\nData di dalam list adalah: \n";
+                Node *currentNode = START;
+                
+                while (currentNode != NULL)
+                {
+                    cout << currentNode->noMhs << endl;
+                    currentNode = currentNode->next;
+                }
+                cout << endl;
+            }
+        }
+
     }
 }
