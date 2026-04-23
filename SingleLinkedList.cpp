20010be39be6304faa7d19b6add14964ee8da086
annisafathul <annisafathul18@gmail.com>
2026-04-23 16:06:43 +0700

    menambahkan boolsearch untuk mencari data

--- SingleLinkedList.cpp
+++ SingleLinkedList.cpp
@@ -79,5 +79,18 @@ class LinkedList
         {
             return (START == NULL);
         }
+
+         bool search(int nim, Node *&previous, Node *&current)
+        {
+            previous = START;
+            current = START;
+
+            while (current != NULL && current->noMhs != nim)
+            {
+                previous = current;
+                current = current->next;
+            }
+            return current != NULL;
+        }
     }
 }
