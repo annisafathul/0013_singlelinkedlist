8aa3c8cec8aa6084be5fbd7c216ab4331a3bc969
annisafathul <annisafathul18@gmail.com>
2026-04-23 15:39:07 +0700

    Menentukan nilai pertama

--- SingleLinkedList.cpp
+++ SingleLinkedList.cpp
@@ -39,3 +39,19 @@ class LinkedList
             START = nodeBaru;   
             return;
         }
+
+        node * previous = START;
+        node * current = START; 
+
+         if (START == NULL || START->noMhs >= nodeBaru->noMhs)
+        {
+            if (START != NULL && START->noMhs == nodeBaru->noMhs)
+            {
+                cout << "\nDuplikasi noMhs tidak diijinkan\n";
+                return;
+            }
+        
+            nodeBaru->next = START;
+            START = nodeBaru;   
+            return;
+        }
